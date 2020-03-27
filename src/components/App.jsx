8960b79d2e35862a32ad0358b34acad254e96932
Ariel Sierra <arielsierragarcia@gmail.com>
2020-03-26 20:28:27 -0600

    Make changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,3 @@ export default App;
 
 //Reference
 // isLoggedIn ? <h1>Hello</h1> : <Login />
-
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
